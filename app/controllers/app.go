d933328822e18585f9f17f0e2e01f07903c000ee
hild1985 <hild1985@gmail.com>
2015-10-23 23:22:34 +0700

    no message

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,11 +1,13 @@
 package controllers
 
 import "github.com/revel/revel"
+import "labix.org/v2/mgo"
 
 type App struct {
 	*revel.Controller
 }
 
-func (c App) Index() revel.Result {
+func (c App) Index() revel.Result {	
+	//db = connect("192.168.59.103:27017/myDatabase")
 	return c.Render()
 }
